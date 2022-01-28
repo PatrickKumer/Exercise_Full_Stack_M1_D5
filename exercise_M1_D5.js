75d160576279ac4609781a509a8ac9b87a196ca1
PatrickKumer <patrick.kumer@gmail.com>
2022-01-28 15:48:22 +0100

    Solved exercise 1

--- exercise_M1_D5.js
+++ exercise_M1_D5.js
@@ -1,3 +1,9 @@
+const printTitle = function(nameOfTitle){
+    console.log(`\n-----------${nameOfTitle}---------`)
+}
+
+
+
 /*
 ASSIGNMENT RULES
 - All the answers must be written in JavaScript
@@ -12,65 +18,89 @@ ASSIGNMENT RULES
  Write a function called "area" which receives 2 parameters (l1, l2) and calculates the area of the associated rectangle.
 */
 
-/* WRITE YOUR ANSWER HERE */
+printTitle("Exercise 1")
+
+function rectangle (l1, l2){
+    let resultOfRectangle = l1 * l2
+    return resultOfRectangle
+}
+
+let result = rectangle (10, 10)
+console.log("The area of and rectangle is:", result)
+
 
 /* EXERCISE 2
  Write a function called "crazySum" which receives two integers as parameters.
  It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
 */
 
-/* WRITE YOUR ANSWER HERE */
+printTitle("Exercise 2")
+
+let result1 = function crazySum(){
+    if (a + b){
+        return crazySum
+    } else if (a + b === a + a){
+        return (a + b * 3)
+    }
+}
+
+result1 = crazySum(3, 2)
+console.log(crazySum)
 
 /* EXERCISE 3
  Write a function called "crazyDiff" that computes the absolute difference between a given number and 19.
  It should return triple their absolute difference if the given number is greater than 19.
 */
 
-/* WRITE YOUR ANSWER HERE */
+printTitle("Exercise 3")
+
+//function crazyDiff
 
 /* EXERCISE 4
  Write a function called "boundary" which accept an integer parameter n and returns true if n is within 20 and 100 (included) or if n it's equal to 400.
 */
 
-/* WRITE YOUR ANSWER HERE */
+printTitle("Exercise 4")
+
+//function boundary (n)
 
 /* EXERCISE 5
  Write a function called "strivify" which accepts a string as a parameter.
  It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR ANSWER HERE */
+printTitle("Exercise 5")
 
 /* EXERCISE 6
  Write a function called "check3and7" which accepts a positive number as a parameter and checks if it is a multiple of 3 or a multiple of 7.
  HINT: Modulus Operator
 */
 
-/* WRITE YOUR ANSWER HERE */
+printTitle("Exercise 6")
 
 /* EXERCISE 7
  Write a function called "reverseString" which programmatically reverses a given string (es.: Strive => evirtS).
 */
 
-/* WRITE YOUR ANSWER HERE */
+printTitle("Exercise 7")
 
 /* EXERCISE 8
  Write a function called "upperFirst" which capitalizes the first letter of each word of a given string passed as a parameter.
 */
 
-/* WRITE YOUR ANSWER HERE */
+printTitle("Exercise 8")
 
 /* EXERCISE 9
  Write a function called "cutString" which creates a new string without the first and last character of a given string passed as a parameter.
 */
 
-/* WRITE YOUR ANSWER HERE */
+printTitle("Exercise 9")
 
 /* EXERCISE 10
  Write a function called "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
 */
 
-/* WRITE YOUR ANSWER HERE */
+printTitle("Exercise 10")
 
 /* WHEN YOU ARE FINISHED
  Commit and push the code to your personal GitHub repository; then post the link of your commit on the Homework section of today's Eduflow.
